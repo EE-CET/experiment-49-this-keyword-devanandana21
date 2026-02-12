ef051b91370a707cea7570462f15f3865b9d49cc
devanandana21 <tve24el018@cet.ac.in>
2026-02-12 20:57:12 +0530

    Update ThisKeywordDemo.java

--- src/ThisKeywordDemo.java
+++ src/ThisKeywordDemo.java
@@ -12,7 +12,7 @@ class ThisDemo {
     }
 }
 
-public class Main {
+public class ThisKeywordDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int inputValue = sc.nextInt();
